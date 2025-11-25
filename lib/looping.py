50d10065e28a18109efa50589ab28e686d75538c
Kilroy-creator <royhenry877@gmail.com>
2025-11-25 03:02:37 +0300

    first

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    """Count down from 10 to 1, then print Happy New Year!"""
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    """Return a list of squared integers using list comprehension"""
+    return [num ** 2 for num in int_list]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    """Print numbers 1-100, with Fizz/Buzz/FizzBuzz for multiples"""
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
